4fcb6eb567da4139630026103b3b4a9b9d500dbe
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-20 15:14:13 +0530

    make input based on state.style

--- src/Components/InputSection/index.tsx
+++ src/Components/InputSection/index.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import GameContext from "../../Contexts/gameContext";
 import ColorCirle from "../ColorCircle";
 import Button from "./components/Button";
 import Output from "./components/Output";
 
 const InputSection = () => {
+  const { state } = useContext(GameContext);
+
   return (
     <div className="input-section">
-      <ColorCirle style={{ backgroundColor: "white" }} />
-      <ColorCirle style={{ backgroundColor: "white" }} />
-      <ColorCirle style={{ backgroundColor: "white" }} />
-      <ColorCirle style={{ backgroundColor: "white" }} />
+      <ColorCirle style={state.styles[0]} />
+      <ColorCirle style={state.styles[1]} />
+      <ColorCirle style={state.styles[2]} />
+      <ColorCirle style={state.styles[3]} />
       <Button />
       <Output />
     </div>
