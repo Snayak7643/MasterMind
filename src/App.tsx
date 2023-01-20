5f0d3461731439cc80d60bbc47a39142f7433f9c
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-20 15:09:55 +0530

    put state, dispatch in context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useReducer } from "react";
 import Heading from "./Components/Heading";
 import InputSection from "./Components/InputSection";
 import SelectionBox from "./Components/SelectionBox";
-import { INPUT_SECTIONS } from "./constants";
+import { INITIAL_STATE, INPUT_SECTIONS } from "./constants";
+import GameContext from "./Contexts/gameContext";
+import gameReducer from "./Reducers/gameReducer";
 
 function App() {
+  const [state, dispatch] = useReducer(gameReducer, INITIAL_STATE);
+
   return (
-    <main>
-      <Heading />
-      <SelectionBox />
-      {INPUT_SECTIONS.map((no) => {
-        return <InputSection key={no} />;
-      })}
-    </main>
+    <GameContext.Provider value={{ state, dispatch }}>
+      <main>
+        <Heading />
+        <SelectionBox />
+        {INPUT_SECTIONS.map((no) => {
+          return <InputSection key={no} />;
+        })}
+      </main>
+    </GameContext.Provider>
   );
 }
 
