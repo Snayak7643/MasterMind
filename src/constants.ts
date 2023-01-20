16ebed4e63aac28eb11264bb662c95b807d6ef53
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-20 15:00:22 +0530

    initial state created

--- src/constants.ts
+++ src/constants.ts
@@ -8,3 +8,18 @@ export const COLORS = [
 ];
 
 export const INPUT_SECTIONS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export type StateType = {
+  selectedColor: number;
+  styles: { backgroundColor: string }[];
+};
+
+export const INITIAL_STATE: StateType = {
+  selectedColor: 7,
+  styles: [
+    { backgroundColor: "white" },
+    { backgroundColor: "white" },
+    { backgroundColor: "white" },
+    { backgroundColor: "white" },
+  ],
+};
