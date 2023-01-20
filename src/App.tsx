867cde5c91e89d0d20a98cba28dc7c7ed7ca74e8
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-20 12:55:58 +0530

    colorCircle created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ColorCirle from "./Components/ColorCircle";
 
 function App() {
-  return <h1> MasterMind </h1>;
+  return (
+    <>
+      <h1> MasterMind </h1>
+      <ColorCirle />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/ColorCircle.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ColorCirle = () => {
+  return <button className="color-circle"></button>;
+};
+
+export default ColorCirle;
