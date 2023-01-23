e06615efbe63cf49fe9d4d8622eea54723bae72a
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-23 19:16:13 +0530

    modal created

---
+++ src/Components/Modal.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+    <div className="overlay">
+      <div className="overlay-box">
+        <h3>Congratulations!! Modal</h3>
+        <button className="okay">Replay</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
